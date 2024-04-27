8a0742a2ca2a18e216c40aeab1dbe5d49f698d30
ostryJR <mikolaj.p.ostrowski@gmail.com>
2024-04-28 00:40:35 +0200

    finished :)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from PIL import Image
+from PIL import Image, ImageOps
 import os
 import config as cfg
+import shutil
 
 def makeFolder(name):
     path = f'{cfg.pathMade}{name}'
@@ -25,9 +26,18 @@ def scanForNewImages():
 
 def makeImage(imageName, size):
     #print(f"Making image {imageName} with size {size}")
-    image = Image.open(f'{cfg.pathToMake}{imageName}')
-    image = image.resize((size, size))
-    image.save(f'{cfg.pathMade}/{imageName.split('.')[0]}/{imageName.split('.')[0]}{size}.png')
+    imageRaw = Image.open(f'{cfg.pathToMake}/{imageName}')
+    image = imageRaw.convert('RGBA')
+    if image.size[0] > image.size[1]:
+        scale = image.size[0]/size
+        image = image.resize((int(image.size[0]/scale), int(image.size[1]/scale)))
+    else:
+        scale = image.size[1]/size
+        image = image.resize((int(image.size[0]/scale), int(image.size[1]/scale)))
+
+    image2 = Image.new('RGBA', (size, size))
+    image2.paste(image, (int((size - image.size[0]) / 2), int((size - image.size[1]) / 2)))
+    image2.save(f'{cfg.pathMade}/{imageName.split('.')[0]}/{imageName.split('.')[0]}{size}.png')
     print(f"Image {imageName.split('.')[0]} of size {size} made")
 
 if __name__ == "__main__":
@@ -35,6 +45,7 @@ if __name__ == "__main__":
 
     for image in imagesToMake:
         makeFolder(image.split('.')[0])
+        shutil.copyfile(image, f'{cfg.pathMade}/{image.split('.')[0]}/{image.split('.')[0]}Original.png')
         for size in cfg.sizes:
             makeImage(image, size)
             pass
